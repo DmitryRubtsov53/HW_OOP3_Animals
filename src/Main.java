4bbe9e587c11928b66a4b0358efdd70431a83277
DmitryRubtsov53 <2578390@mail.ru>
2022-10-27 11:58:03 +0500

    Start doing HW of lessen OOP3 Animals.

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Домашнее задание урока ООР3 Животные *********************************************" + "/n");
+
+    }
+}
